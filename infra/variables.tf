350cc780ec1d3e289a559ed9732c8fab42967480
chik0di <chikodiobu@gmail.com>
2025-11-29 05:09:27 +0100

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -12,3 +12,23 @@ variable "environment" {
   description = "Environment (dev, staging, prod)"
   type        = string
 }
+
+variable "snowflake_iam_user_arn" {
+  description = "snowflake generated user ARN"
+  type        = string
+}
+
+variable "snowflake_external_id" {
+  description = "Snowflale generated eternal id"
+  type        = string
+}
+
+variable "bucket_resource_arn" {
+  description = "ARN of S3 bucket"
+  type        = string
+}
+
+variable "bucket_objects_arn" {
+  description = "ARN of S3 bucket with a forward slash and an asterisk to include all of its objects"
+  type        = string
+}
