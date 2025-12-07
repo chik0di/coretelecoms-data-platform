1590422cc5dd592cfa51092bc1b9e5e3ec66e0c7
chik0di <chikodiobu@gmail.com>
2025-12-07 17:53:59 +0100

    Update postgres utils: better connection handling

--- src/utils/postgres_utils.py
+++ src/utils/postgres_utils.py
@@ -1,25 +1,44 @@
-import boto3
+from airflow.providers.amazon.aws.hooks.ssm import SsmHook
 from sqlalchemy import create_engine, text
-import pandas as pd 
-import datetime
 
-def get_db_credentials(param_name):
-    ssm = boto3.client("ssm")
-    response = ssm.get_parameter(Name=param_name, WithDecryption=True)
-    return response["Parameter"]["Value"]
+def get_db_credentials(return_only=None):
+    """
+    Args:
+        return_only: If specified, return only that credential
+                    Options: 'host', 'database', 'user', 'password', 'port', 'schema'
+    """
+    ssm = SsmHook(aws_conn_id="aws_source_bucket_conn")
+    
+    credentials = {
+        'host': ssm.get_parameter_value(parameter="/coretelecomms/database/db_host"),
+        'database': ssm.get_parameter_value(parameter="/coretelecomms/database/db_name"),
+        'user': ssm.get_parameter_value(parameter="/coretelecomms/database/db_username"),
+        'port': ssm.get_parameter_value(parameter="/coretelecomms/database/db_port"),
+        'password': ssm.get_parameter_value(parameter="/coretelecomms/database/db_password"),
+        'schema': ssm.get_parameter_value(parameter="/coretelecomms/database/table_schema_name")
+    }
+    
+    if return_only:
+        return credentials.get(return_only)
+    
+    return credentials
 
-host = get_db_credentials(param_name="/coretelecomms/database/db_host")
-database = get_db_credentials(param_name="/coretelecomms/database/db_name")
-user = get_db_credentials(param_name="/coretelecomms/database/db_username")
-port = get_db_credentials(param_name="/coretelecomms/database/db_port")
-password = get_db_credentials(param_name="/coretelecomms/database/db_password")
-schema = get_db_credentials(param_name="/coretelecomms/database/table_schema_name")
-
-engine = create_engine(
-    f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
-)
+def get_postgres_engine():
+    creds = get_db_credentials()
+    
+    engine = create_engine(
+        f"postgresql+psycopg2://{creds['user']}:{creds['password']}@{creds['host']}:{creds['port']}/{creds['database']}",
+        connect_args={
+            "keepalives"            :1,
+            "keepalives_idle"       :30,
+            "keepalives_interval"   :10,
+            "keepalives_count"      :5
+        }
+    )
+    return engine
 
 def list_webform_tables():
+    schema = get_db_credentials(return_only="schema")
     query = text(f"""
         SELECT table_name 
         FROM information_schema.tables
@@ -27,18 +46,11 @@ def list_webform_tables():
         AND table_name LIKE 'web_form_request_%'
         ORDER BY table_name;
     """)
-
+    engine = get_postgres_engine()
     with engine.connect() as conn:
         rows = conn.execute(query).fetchall()
 
     return [r[0] for r in rows]
-
-
-# s3 = boto3.client("s3")
-# def s3_partition_exists(bucket: str, prefix: str) -> bool:
-#     """Check if any object exists in S3 under the given prefix."""
-#     response = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
-#     return 'Contents' in response
 
 def generate_s3_partitioned_key(prefix: str, table) -> str:
     """
@@ -48,6 +60,5 @@ def generate_s3_partitioned_key(prefix: str, table) -> str:
     tablename = table             
     date_str = tablename.split("_")[3:]  # 2025-11-20
     year, month, day = date_str[0], date_str[1], date_str[2]
-    ts = datetime.datetime.now(datetime.timezone.utc).strftime("%Y%m%dT%H%M%SZ")
-    parquet_name = f"webform_{ts}.parquet"
+    parquet_name = f"{prefix}-{year}-{month}-{day}.parquet"
     return f"{prefix}/{year}/{month}/{parquet_name}"
